b84956090b446199c167d3db88341909566b3e88
Guro Bokum <jiojiajiu@gmail.com>
2019-10-06 17:04:16 +0800

    Implement searching by columns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 
 import './App.css';
 import instructions from './instructions';
@@ -9,17 +10,19 @@ const columns = [
     dataField: 'asm',
     text: 'Asm',
     sort: true,
-    classes: 'fit'
+    filter: textFilter(),
   },
   {
     dataField: 'code',
     text: 'Code',
-    sort: true
+    sort: true,
+    filter: textFilter(),
   },
   {
     dataField: 'alias',
     text: 'Alias',
-    sort: true
+    sort: true,
+    filter: textFilter(),
   },
   {
     dataField: 'stack',
@@ -37,7 +40,7 @@ function App() {
       <header className="App-header">
         <h1>FIFT Spreadsheet</h1>
         <div>Current page contains FIFT codes and instructions for <a href="https://test.ton.org/" target="_blank">TON Blockchain</a></div>
-        <div>Based on <a href="https://test.ton.org/" target="_blank">TVM.pdf</a> and <a href="https://test.ton.org/fiftbase.pdf" target="_blank">FIFTbase.pdf</a></div>
+        <div>Based on <a href="https://test.ton.org/tvm.pdf#subsection.A.2" target="_blank">TVM.pdf</a> and <a href="https://test.ton.org/fiftbase.pdf#appendix.A" target="_blank">FIFTbase.pdf</a></div>
         <div className="alert alert-danger">In progress. Not all codes/instructions are covered</div>
       </header>
 
@@ -46,8 +49,9 @@ function App() {
           keyField='asm'
           hover
           striped
-          bordered={ false }
+          condensed
           data={ instructions }
+          filter={ filterFactory() }
           columns={ columns } />
       </div>
     </div>
